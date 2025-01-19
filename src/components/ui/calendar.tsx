f8ce19829723b8d0a7556003475eb867af7f6e4e
ritik <ritikkharya05@gmail.com>
2025-01-20 02:08:46 +0530

    updated the config.file

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -52,8 +52,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ..._props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ..._props }) => <ChevronRight className="h-4 w-4" />,
+        NavigationPrev: () => <ChevronLeft className="h-4 w-4" />,
+        NavigationNext: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
